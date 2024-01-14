d3c8c0fa622f3c9fedcfd87925c4c6a81eadc093
sidharth <siddharthbajpai2812@gmail.com>
2024-01-14 13:40:58 +0530

    new com

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+//components C:\Users\Sidharth\Desktop\nf\client\src\components\header\Header.jsx
+import Header from './components/header/Header';
+//import Header from './component/header/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+       <div>Hello From Header</div>  
+         )
+}
+export default Header;
